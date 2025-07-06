4c5144acb59d6a2e30f2ba41fad7828a69166548
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-07-07 00:07:44 +0100

    Adding book photo to the details page as well as buttons for favorites, rating, language

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/bookDetail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/bookDetail/components/BlurredImageBackground.kt
@@ -2,24 +2,32 @@ package com.plcoding.bookpedia.book.presentation.book_list.bookDetail.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.book_cover
+import cmp_bookpedia.composeapp.generated.resources.go_back
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
 import org.jetbrains.compose.resources.stringResource
+import androidx. compose. foundation. layout
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
 
 @Composable
 fun BlurredImageBackground(
@@ -76,7 +84,39 @@ fun BlurredImageBackground(
 
             )
         }
+        IconButton(
+            onClick = onBackClick,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(top = 16.dp, start = 16.dp)
+                .statusBarsPadding()
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = stringResource(Res.string.go_back),
+                tint = androidx.compose.ui.graphics.Color.White
+            )
+        }
 
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Spacer(modifier = Modifier.fillMaxHeight(0.15f))
+            ElevatedCard(
+                modifier = Modifier
+                    .width(200.dp)
+                .aspectRatio(2 /3f),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = androidx.compose.ui.graphics.Color.Transparent)
+            ),
+            elevation = CardDefaults.elevatedCardElevation(
+                defaultElevation = 15.dp,
+            ) {
+
+            }
+        }
 
     }
 }
