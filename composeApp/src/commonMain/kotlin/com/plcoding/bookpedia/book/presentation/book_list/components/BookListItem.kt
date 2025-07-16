5274e425528413c09cbee1e672f88293a0373abf
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-07-16 14:48:28 +0100

    Rotation animation for book cover added

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package com.plcoding.bookpedia.book.presentation.book_list.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -16,6 +18,7 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,9 +26,11 @@ import kotlin.math.round
 
 import com.plcoding.bookpedia.book.domain.Book
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.book_error_2
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.LightBlue
 import com.plcoding.bookpedia.core.presentation.PulseAnimation
@@ -75,6 +80,11 @@ fun BookListItem(
                         imageLoadResult = Result.failure(it.result.throwable)
                     }
                 )
+                val painterState by painter.state.collectAsStateWithLifecycle()
+                val transition by animateFloatAsState(
+                    targetValue = if(painterState is AsyncImagePainter.State.Success) 1f else 0f,
+                    animationSpec = tween(800)
+                )
                 when(val result = imageLoadResult) {
                     null -> PulseAnimation(
                         modifier = Modifier.size(60.dp),
@@ -95,6 +105,12 @@ fun BookListItem(
                                     ratio = 0.65f,
                                     matchHeightConstraintsFirst = true
                                     )
+                                .graphicsLayer {
+                                    rotationX = (1f - transition) * 30f
+                                    val scale = 0.8f + (0.2f * transition)
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
                         )
                     }
                 }
